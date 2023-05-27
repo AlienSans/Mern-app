ae4e343779738f4b1aa9b49ff0b8dc92219edffc
AlienSans <aliensans51@gmail.com>
2023-05-27 16:26:09 +0700

    account page and logout

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,19 +1,64 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Navigate, Link, useParams } from "react-router-dom";
 import UserContext from "../UserContext";
+import axios from "axios";
 
 function AccountPage() {
-  const { ready, user } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  const logout = async () => {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  };
 
   if (!ready) {
     return "Loading...";
   }
 
-  if (ready && !user) {
+  if (ready && !user && !redirect) {
     return <Navigate to={"/login"} />;
   }
 
-  return <div>AccountPage {user.name}</div>;
+  const linkClasses = (type = null) => {
+    let clasess = "py-2 px-6";
+    if (type === subpage || (subpage === undefined && type === "profile")) {
+      clasess += " bg-primary text-white rounded-full";
+    }
+    return clasess;
+  };
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My Profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My Bookings
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My Accommodation
+        </Link>
+      </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email}) <br />
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default AccountPage;
