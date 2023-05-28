755a72a906e3738da9d1199cb29ccf13a1583314
AlienSans <aliensans51@gmail.com>
2023-05-29 06:40:50 +0700

    form input

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
 const User = require("./models/userModel");
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
@@ -72,6 +77,30 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads" + newName,
+  });
+  res.json(newName);
+});
+
+const photosMiddleware = multer({ dest: "uploads" });
+app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace("uploads/", ""));
+  }
+  res.json(uploadedFiles);
 });
 
 module.exports = app;
---
+++ server/models/vehicleModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const vehicleSchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: String,
+  type: String,
+  address: String,
+  feature: [String],
+  photos: [String],
+  description: String,
+  checkIn: Number,
+  checkOut: Number,
+  maxGuests: Number,
+  extraInfo: String,
+});
+
+const Vechile = mongoose.model("Vehicle", vehicleSchema);
+
+module.exports = Vechile;
